05bd1df6f3d07be2c267335ae6cd0d74e34428ab
Martin Renteria <renteriamp96@gmail.com>
2020-11-14 23:59:49 -0800

    Built ajax call that grabs information about city weather data and it gets called by the click of the search button. Currently displays city, date, humidity, and wind speed.

--- script.js
+++ script.js
@@ -1,15 +1,17 @@
+//When search button is clicken, the api will bring the data onto the screen 
 $("#searchBtn").on("click", weatherDisplay)
-
-function weatherDisplay() {
+// Displays search data of cities onto weather div container
+function weatherDisplay(e) {
+  e.preventDefault();
 
 var cities = $("#searchInput").val();
 
 var apiKey = "ded885a5140a03bceffcd25035fcab57"
 
-
+// URL links to specific data of desired data
 var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cities + "&APPID=" + apiKey;
 var forecast5 = "https://api.openweathermap.org/data/2.5/forecast?q=" + cities + "&APPID=" + apiKey;
-
+// Performing ajax call for single day weather data 
   $.ajax({
       url: queryURL,
       method: "GET"
@@ -18,14 +20,12 @@ var forecast5 = "https://api.openweathermap.org/data/2.5/forecast?q=" + cities +
 
   var tempF = (response.main.temp - 273.15) * 1.80 + 32;
   var todayDate = moment().format("MM/DD/YYYY");
-  var iconcode = response.weather[0].icon;
-  var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
 
-  //transfer content to HTML
-  $(".city").html("<h2>" + response.name + " " + todayDate + "</h2>");
-  $("#wicon").attr("src", iconurl);
-  $(".temp").text("Temperature: " + tempF.toFixed(2) + " °F");
-  $(".humidity").text("Humidity: " + response.main.humidity + "%");
-  $(".wind").text("Wind Speed: " + response.wind.speed + " MPH");
-  })
-}
+  // Draws APi Data to weather div container
+  $(".city").html("<h3>" + response.name + " " + todayDate + "</h3>");
+  $(".resultTemp").text("Temperature: " + tempF.toFixed(2) + " °F");
+  $(".resultHum").text("Humidity: " + response.main.humidity + "%");
+  $(".resultWind").text("Wind Speed: " + response.wind.speed + " MPH");
+  })}
+
+  
